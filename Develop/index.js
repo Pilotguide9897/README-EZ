7be79464eb4bab4347f5ea417b4909d8be3b5831
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-03-10 19:17:38 -0400

    added prompts and questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,102 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+const {error} = require('console');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What would you like to title your project?',
+        name: 'title'
+    },
+    {
+        type: 'editor',
+        message: 'Please provide a description of your application.',
+        name: 'description'
+    },
+    {
+        type: 'editor',
+        message: 'Please provide instructions on how to install your application',
+        name: 'installation'
+    },
+    {
+        type: 'editor',
+        message: 'Please provide an overview of how to use your application.',
+        name: 'usage'
+    },
+    {
+        type: 'list',
+        message: 'Please select the appropriate license type.',
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', "BSD2-Clause 'simplified' License", "BSD 3-Clause 'New' or 'Revised' License", 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+        name: 'license'
+    },
+    {
+        type: 'editor',
+        message: 'Please provide guidelines on how users can contribute to the project.',
+        name: 'contributing'
+    },
+    {
+        type: 'editor',
+        message: 'Provide a description of any testing that has been performed prior to deployment of this application.',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'ghUsername'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email'
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const dataToWrite = `#${answers.title}\n
+##Description\n
+    ${}\n
+##Table of Contents\n
+    [Installation](##Installation)\n
+    [Usage](##Usage)\n
+    [Contributing](##Contributing)\n
+    [Tests](##Tests)\n
+    [Questions](##Questions)\n
+##Installation\n
+    ${}\n
+##Usage\n
+    ${}\n
+##Contributing\n
+    ${}\n
+##Tests\n
+    ${}\n
+##Questions\n
+   If you have any questions concerning this application, do not hesitate to reach me at ${answers.email}.\n
+   You may also view my GitHub profile at ${answers.ghUsername}.`;
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to write README file
+fs.writeToFile('readme.md', dataToWrite, function (error) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Readme Generated!');
+    }
+});
+
+// // TODO: Create a function to initialize app
+// // function init() {
+// // }
+
+// // Function call to initialize app
+// // init();
+
+inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+        return answers
+    })
+    .catch((err) =>{
+        console.error(error);
+    });
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,89 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// TODO: Create a function that returns a license badge based on which license is passed in. If there is no license, return an empty string
+
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'Apache License 2.0':
+      return `https://img.shields.io/badge/License-Apache%202.0-blue.svg`;
+    case 'GNU General Public License v3.0':
+      return `https://img.shields.io/badge/License-GPLv3-blue.svg`;
+    case 'MIT License':
+      return `https://img.shields.io/badge/License-MIT-yellow.svg`;
+    case "BSD2-Clause 'simplified' License":
+      return `https://img.shields.io/badge/License-BSD%202--Clause-orange.svg`;
+    case "BSD 3-Clause 'New' or 'Revised' License":
+      return `https://img.shields.io/badge/License-BSD%203--Clause-blue.svg`;
+    case 'Boost Software License 1.0':
+      return `https://img.shields.io/badge/License-Boost%201.0-lightblue.svg`;
+    case 'Creative Commons Zero v1.0 Universal':
+      return `https://img.shields.io/badge/License-CC0%201.0-green.svg`;
+    case 'Eclipse Public License 2.0':
+      return `https://img.shields.io/badge/License-EPL%202.0-red.svg`;
+    case 'GNU Affero General Public License v3.0':
+      return `https://img.shields.io/badge/License-AGPL%20v3-blue.svg`;
+    case 'GNU General Public License v2.0':
+      return `https://img.shields.io/badge/License-GPL%20v2-blue.svg`;
+    case 'GNU Lesser General Public License v2.1':
+      return `https://img.shields.io/badge/License-LGPL%20v2.1-blue.svg`;
+    case 'Mozilla Public License 2.0':
+      return `https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg`;
+    case 'The Unlicense':
+      return `https://img.shields.io/badge/license-Unlicense-blue.svg`;
+    default:
+      return '';
+  }
+}
+
+// function renderLicenseBadge(license) {
+//   switch (license) {
+//     case 'Apache License 2.0':
+//       return `https://img.shields.io/badge/License-Apache%202.0-blue.svg`;
+//     case 'GNU General Public License v3.0':
+//       return `https://img.shields.io/badge/License-GPLv3-blue.svg`;
+//     case 'MIT License':
+//       return `https://img.shields.io/badge/License-MIT-yellow.svg`;
+//     case "BSD2-Clause 'simplified' License":
+//       return `https://img.shields.io/badge/License-BSD%202--Clause-orange.svg`;
+//     case "BSD 3-Clause 'New' or 'Revised' License":
+//       return `https://img.shields.io/badge/License-BSD%203--Clause-blue.svg`;
+//     case 'Boost Software License 1.0':
+//       return `https://img.shields.io/badge/License-Boost%201.0-lightblue.svg`;
+//     case 'Creative Commons Zero v1.0 Universal':
+//       return `https://img.shields.io/badge/License-CC0%201.0-green.svg`;
+//     case 'Eclipse Public License 2.0':
+//       return `https://img.shields.io/badge/License-EPL%202.0-red.svg`;
+//     case 'GNU Affero General Public License v3.0':
+//       return `https://img.shields.io/badge/License-AGPL%20v3-blue.svg`;
+//     case 'GNU General Public License v2.0':
+//       return `https://img.shields.io/badge/License-GPL%20v2-blue.svg`;
+//     case 'GNU Lesser General Public License v2.1':
+//       return `https://img.shields.io/badge/License-LGPL%20v2.1-blue.svg`;
+//     case 'Mozilla Public License 2.0':
+//       return `https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg`;
+//     case 'The Unlicense':
+//       return `https://img.shields.io/badge/license-Unlicense-blue.svg`;
+//     default:
+//       return '';
+//   }
+// }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// function generateMarkdown(data) {
+//   return `# ${data.title}
 
-`;
-}
+// `;
+// }
 
-module.exports = generateMarkdown;
+module.exports = {
+renderLicenseBadge,
+renderLicenseLink,
+renderLicenseSection,
+generateMarkdown,
+};
