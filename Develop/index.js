82463644f6d74ad5d6e2e6dd5e6697a41f4b8184
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-03-16 11:12:08 -0300

    commit before adding license badge

--- Develop/index.js
+++ Develop/index.js
@@ -29,7 +29,7 @@ const questions = [
     {
         type: 'list',
         message: 'Please select the appropriate license type.',
-        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', "BSD2-Clause 'simplified' License", "BSD 3-Clause 'New' or 'Revised' License", 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', "BSD2-Clause 'simplified' License", "BSD 3-Clause 'New' or 'Revised' License", 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense', 'I do not want a license'],
         name: 'license'
     },
     {
